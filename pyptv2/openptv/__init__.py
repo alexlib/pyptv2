57561bc5727ed0e1aabd1fa0544c48078b167d88
Alex Liberzon <alexlib@users.noreply.github.com>
2025-03-01 22:20:33 +0200

    Add openptv subdirectory structure for OpenPTV Python integration

---
+++ pyptv2/openptv/__init__.py
@@ -0,0 +1,8 @@
+"""OpenPTV Python library for Particle Tracking Velocimetry.
+
+This package contains the Python implementation of the OpenPTV
+algorithms for 3D particle tracking velocimetry. It serves as 
+the computational core for PyPTV2.
+"""
+
+__all__ = []  # Will be populated as modules are implemented
